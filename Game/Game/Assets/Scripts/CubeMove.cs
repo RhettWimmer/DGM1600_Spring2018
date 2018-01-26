1dee99c7504c8c7f6de8932729fd0b06cde31f3d
Unknown <thedovahbrine@gmail.com>
2018-01-25 18:29:37 -0700

    Problem solving

--- Game/Game/Assets/Scripts/CubeMove.cs
+++ Game/Game/Assets/Scripts/CubeMove.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeMove : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed;
+	public float jumpHeight;
 
-	public float transX;
+	/* public float transX;
 	public float transY;
 	public float transZ;
 
 	public float rotX;
 	public float rotY;
-	public float rotZ;
+	public float rotZ; */
+
+		public Vector3 move;
+		public Vector3 rotate;
 
 /*
 	public int number = 10;
@@ -25,9 +31,21 @@ public class CubeMove : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.Translate(transX, transY, transZ);
-		transform.Rotate(rotX, rotY, transZ);
-		
+	 void Update () {
+		 var j = Input.GetAxis("Jump")*Time.deltaTime * jumpHeight;
+		 var y = Input.GetAxis("Horizontal")*Time.deltaTime * turnSpeed;
+		 var z = Input.GetAxis("Vertical")*Time.deltaTime * moveSpeed;
+
+		 transform.Rotate(0,y,0);
+		 transform.Translate(0,0,z);
+		 transform.Translate(0,j,0);
+
+
+
+		/*transform.Translate(transX, transY, transZ);
+		transform.Rotate(rotX, rotY, transZ); */
+
+	/*transform.Translate(move * Time.deltaTime);
+	transform.Rotate(rotate * Time.deltaTime);*/
 	}
 }

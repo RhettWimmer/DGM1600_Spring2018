ac50e77ad4756f69a724bab8c69290db41a4360e
Unknown <thedovahbrine@gmail.com>
2018-01-25 19:24:53 -0700

    Finshed Controller

--- Game/Game/Assets/Scripts/CubeMove.cs
+++ Game/Game/Assets/Scripts/CubeMove.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeMove : MonoBehaviour {
-	public float moveSpeed;
-	public float turnSpeed;
-	public float jumpHeight;
+	// public float moveSpeed;
+	// public float turnSpeed;
+	// public float jumpHeight;
 
 	/* public float transX;
 	public float transY;
@@ -17,6 +17,8 @@ public class CubeMove : MonoBehaviour {
 
 		public Vector3 move;
 		public Vector3 rotate;
+		public float moveSpeed;
+		
 
 /*
 	public int number = 10;
@@ -32,20 +34,37 @@ public class CubeMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	 void Update () {
-		 var j = Input.GetAxis("Jump")*Time.deltaTime * jumpHeight;
-		 var y = Input.GetAxis("Horizontal")*Time.deltaTime * turnSpeed;
-		 var z = Input.GetAxis("Vertical")*Time.deltaTime * moveSpeed;
+		//  var j = Input.GetAxis("Jump")*Time.deltaTime * jumpHeight;
+		//  var y = Input.GetAxis("Horizontal")*Time.deltaTime * turnSpeed;
+		//  var z = Input.GetAxis("Vertical")*Time.deltaTime * moveSpeed;
 
-		 transform.Rotate(0,y,0);
-		 transform.Translate(0,0,z);
-		 transform.Translate(0,j,0);
+		//  transform.Rotate(0,y,0);
+		//  transform.Translate(0,0,z);
+		//  transform.Translate(0,j,0);
 
+
+		 if(Input.GetKey(KeyCode.W)){
+			 transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		 }
+
+		 if(Input.GetKey(KeyCode.S)){
+			 transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+		 }
+		if(Input.GetKey(KeyCode.A)){
+			 transform.Translate(Vector3.left*moveSpeed*Time.deltaTime);
+		 }
+
+		 if(Input.GetKey(KeyCode.D)){
+			 transform.Translate(Vector3.right*moveSpeed*Time.deltaTime);
+		 }
+
+		 
 
 
 		/*transform.Translate(transX, transY, transZ);
 		transform.Rotate(rotX, rotY, transZ); */
 
-	/*transform.Translate(move * Time.deltaTime);
-	transform.Rotate(rotate * Time.deltaTime);*/
+	// transform.Translate(move * Time.deltaTime);
+	// transform.Rotate(rotate * Time.deltaTime);
 	}
 }

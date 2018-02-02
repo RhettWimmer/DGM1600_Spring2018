36343c55278588a5ba09f65b0b59f8ae2cd69a2d
Unknown <thedovahbrine@gmail.com>
2018-02-01 19:55:40 -0700

    Leaving class added shoot

---
+++ Game/Game/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public int damage = 1; //how much damage it does
+	public int time = 5; //how long it lives before leaving
+
+	void Start () {
+		StartCoroutine(DestroyBullet());
+	}
+	
+	void Update () {
+
+		}
+		
+		IEnumerator DestroyBullet(){
+			yield return new WaitForSeconds(time); 
+			Destroy(gameObject);	
+	}
+}

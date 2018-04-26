907f2a3b7ffeffa0733be9158dd14d6e95e2fb92
Unknown <thedovahbrine@gmail.com>
2018-04-26 08:44:00 -0600

    Added Main Menu

---
+++ Game/Game/Assets/Scripts/WolfAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAI : MonoBehaviour {
+	public float moveSpeed;
+	public Transform target;
+	public int damage;
+
+	void OnTriggerStay(Collider other){
+		if(other.gameObject.name == "Player"){
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+	}
+
+
+void OnCollisionEnter(Collision other){
+	print("Wolf is attacking!");
+	var hit = other.gameObject;
+	var health = hit.GetComponent<PlayerHeath>();
+
+	other.gameObject.GetComponent<PlayerHeath>();
+
+	if(health != null){
+		health.TakeDamage(damage);
+		}
+	}	
+}
+
